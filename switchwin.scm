b7670d9ff21aab50fece1cab175dc198db5cf9b2
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-07-20 13:03:23 -0700

    is-emacs -> window-is-emacs? now handles no focused window

--- switchwin.scm
+++ switchwin.scm
@@ -2,22 +2,23 @@
 -e main -s
 !#
 (load "./util.scm")
-(define (is-emacs)
-  (string= (hc 'get_attr 'clients.focus.class)
-	   "Emacs"))
+(define (window-is-emacs?)
+  (let ((res (hc 'get_attr 'clients.focus.class)))
+    (and (string? res) ; Since it can simply return an #:<eof>
+         (string= res "Emacs"))))
 
 (define (emacs-winmove direction)
   (system* "emacsclient" "--eval" (string-append "(windmove-"   direction ")")))
 
 (define (mvnt-cmd dir)
-  (if (is-emacs)
+  (if (window-is-emacs?)
       (emacs-winmove dir)))
 
 ;; Switch windows in emacs as if they are herbstluft frames
 (define (main args)
   (let* ((direction (cadr args))
         (fallback (lambda (x) (hc 'focus x))))
-    (if (is-emacs)
+    (if (window-is-emacs?)
         (unless (zero? (emacs-winmove direction))
           (fallback direction))
         (fallback direction))))
